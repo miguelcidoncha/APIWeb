cfec88a1385980c5d22640b95e8341e6f1722779
miguelcidoncha <cidoncha1996@gmail.com>
2023-07-18 13:49:21 +0200

    Add project files.

---
+++ WebApplication1/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Entities;
+using WebApplication1.IServices;
+
+namespace WebApplication1.Services
+{
+    public class ProductService : BaseContextService, IProductService
+    {
+        public ProductService(ServiceContext serviceContext) : base(serviceContext) 
+        {
+        }
+
+        public int insertProduct(ProductItem productItem)
+        {
+            _serviceContext.Products.Add(productItem);
+            _serviceContext.SaveChanges();
+            return productItem.Id;
+        }
+    }
+}
