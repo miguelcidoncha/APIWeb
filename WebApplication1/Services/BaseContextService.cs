a5a5b732125d087033f4a2f59c645c1e19c5b788
isabellozano37 <isabeloficial37@gmail.com>
2023-07-24 19:19:09 +0200

    changes

--- WebApplication1/Services/BaseContextService.cs
+++ WebApplication1/Services/BaseContextService.cs
@@ -5,16 +5,9 @@ namespace WebApplication1.Services
     public abstract class BaseContextService
     {
         protected readonly ServiceContext _serviceContext;
-        private ServiceContext serviceContext;
-
         protected BaseContextService(ServiceContext serviceContext)
         {
             _serviceContext = serviceContext;
-        }
-
-        protected BaseContextService(ServiceContext serviceContext)
-        {
-            this._serviceContext = ServiceContext;
         }
     }
 }
